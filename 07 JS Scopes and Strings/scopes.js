0e39599622acb09d1dfdfc54d320483f5a63814a
yogi365 <sharma.yogendra@yahoo.com>
2024-08-31 14:21:37 +0530

    DOM intro

---
+++ 07 JS Scopes and Strings/scopes.js
@@ -0,0 +1,31 @@
+// var a = 20;
+// let b = 10;
+
+// function abc(){
+//     var a = 30;
+//     let b = 30;
+
+//     if(true){
+//         var c = 30;
+//         let d = 40;
+//         console.log(a,b,c,d);
+//     }
+
+    
+// }
+// abc();
+
+var abc = function(){
+    console.log('First abc');
+}
+
+
+// function abc(){
+//     console.log('First abc');
+// }
+
+function abc(){
+    console.log('second abc');
+}
+
+abc();
---
+++ 07 JS Scopes and Strings/strings.js
@@ -0,0 +1,96 @@
+// var str = "This is a string";
+// var strObj = new String("This is a string");
+// var str1 = 'This is a string';
+// var str2 = `This is a string`;
+// var firstName = "Abc";
+// var dob = "11/01/2024";
+
+// var str = "Hello! "+firstName+" your date of birth is"+dob+". Nice meeting you.";
+// var strInterpolation = `Hello! ${firstName} your date of birth is ${dob}. Nice meeting you.`;
+// var sum = `Sum of 2  & 12 is ${2+12}`;
+
+
+// var str = "This is a backslash // /"/"";
+// var str =`This is a backslash ""  '' `
+
+// var str = "This is backlash '' "
+// var str = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.";
+
+// length
+// console.log(str.length)
+
+// chartAt()
+// console.log(str.charAt(3));
+
+// slice(start,end)
+
+// var sliced = str.slice(2,10);
+// var sliced = str.slice(-10,-2);
+// console.log(sliced)
+
+// substring(start,end)
+// var substr = str.substring(2,10);
+// console.log(substr);
+
+// substr(start,length)
+// var _subStr = str.substr(2,10);
+// console.log(_subStr);
+
+// toUpperCase // toLowerCase
+
+var str = "This is a string";
+// var str2 = str.toUpperCase();
+// var str3 = str2.toLowerCase();
+// console.log(str2);
+// console.log(str3);
+
+
+// concat()
+
+// var str1 = "This is first String";
+// var str2 = "This is second string";
+// var str = str1.concat(str2);
+// var str = str1 + str2;
+// console.log(str);
+
+// trim()
+// var str = "          This is a string"
+// str.trim();
+// console.log(str.trim());
+// console.log(str.trimStart());
+// console.log(str.trimEnd());
+
+// padStart()  // padEnd()
+// var str = "52";
+// var paddedStr = st/r.padStart(4,"01");
+// var paddedStr = str.padEnd(4,"0");
+// console.log(paddedStr);
+
+
+// replace()
+// var str = "It's raining heavily, heavily, heavily".replace('heavily','lightly');
+// var newStr = str.replace('heavily','lightly');
+// var newStr = str.replaceAll('heavily','lightly');
+// console.log(str);
+// console.log(newStr);
+
+// indexOf()
+// var str = "It's raining heavily, heavily, heavily";
+// var index = str.indexOf("heavily");
+// console.log(index);
+
+// lastIndexOf()
+// var index = str.lastIndexOf("heavily");
+// console.log(index);
+// search()
+
+// var str = "It's raining heavily, heavily, heavily";
+
+// var searched = str.search(/heavily/g);
+// console.log(searched);
+
+// includes()
+
+// var str = "It's raining heavily, heavily, heavily";
+// var includedValue = str.includes("pain");
+// console.log(includedValue)
