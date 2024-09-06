c58ac6e17cb9eeefae2f075f87f5e796a073312b
yogi365 <sharma.yogendra@yahoo.com>
2024-09-06 09:39:53 +0530

    Calculate bill assignment

---
+++ 10 JS Object - Calculate BIll 06-09/demo.js
@@ -0,0 +1,45 @@
+const employee = [
+    {
+        id:1,
+        name:"James",
+        days:30,
+        salaryId:2
+    },
+    {
+        id:2,
+        name:"Nas",
+        days:29,
+        salaryId:1
+    }
+]
+
+const salary = [
+    {
+        salaryId:1,
+        gross:{
+            base:58000,
+            hra:15000
+        },
+        deduction:{
+            tds:5000,
+            pf:1500
+        }
+    },
+
+    {
+        salaryId:2,
+        gross:{
+            base:88000,
+            hra:25000
+        },
+        deduction:{
+            tds:10000,
+            pf:1500
+        }
+    }
+]
+
+function calculateSalary(){
+
+}
+calculateSalary()
